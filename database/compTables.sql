c64c26cc9cb017426443dc8ef7f779f0095e0a6c
Sallai András <termih@gmail.com>
2019-02-17 15:33:50 +0100

    Modify table script

--- database/compTables.sql
+++ database/compTables.sql
@@ -5,17 +5,17 @@ create table lessons (
 	description text,
 	enabled boolean default 1,
 	categoryId int
-)
+);
 
 create table solutions (
 	id int not null primary key auto_increment,
 	solution text,
 	categoryId int
-)
+);
 
 create table categories (
 	id int not null primary key auto_increment,
 	name varchar(100),
 	enabled boolean default 1
-)
+);
 
